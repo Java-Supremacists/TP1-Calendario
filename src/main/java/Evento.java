4338d7f5f40ef121ab8eafa8e66f1f4cf88446cb
Lima Limon <torsi@fi.uba.ar>
2023-04-08 17:04:55 -0300

    Anado algoritmo para saber si un evento cae un dia en especifico. 3 hardcodeado para testear

--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -1,17 +1,29 @@
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit; //Libreria para formatear dias en LocalDateTime
 import java.util.ArrayList;
 
 public class Evento extends Activities {
 	//--------- Atributos ---------
 	private LocalDateTime arranque;
 	private LocalDateTime termina;
+	private final int repeticion;
 	//--------- Atributos ---------
 	//--------- Constructores ---------
 	public Evento(String name, String description, ArrayList<LocalDateTime> alarm, boolean isComplete, LocalDateTime arranque, LocalDateTime termina) {
 		super(name, description, alarm, isComplete);
 		this.arranque = arranque;
 		this.termina = termina;
+		this.repeticion = 3;
 	}
+
+
+	//Constructor con frecuencia
+	// public Evento(String name, String description, ArrayList<LocalDateTime> alarm, boolean isComplete, LocalDateTime arranque, LocalDateTime termina, String frecuencia) {
+	// 	super(name, description, alarm, isComplete);
+	// 	this.arranque = arranque;
+	// 	this.termina = termina;
+	// 	this.frecuencia = frecuencia;
+	// }
 	//--------- Constructores ---------
 	//--------- Metodos ---------
 	@Override
@@ -25,6 +37,13 @@ public class Evento extends Activities {
 	@Override
 	public LocalDateTime cuandoTermina() {
 		return this.termina;
+	}
+
+	public boolean caeElDia(LocalDateTime diaEspecifico){
+	    long cantDiasHastaDiaPedido = this.arranque.until(diaEspecifico, ChronoUnit.DAYS);
+	    boolean eventoCaeElDiaPedidio = (cantDiasHastaDiaPedido % this.repeticion == 0);
+	    return eventoCaeElDiaPedidio;
+
 	}
 	//--------- Metodos ---------
 }
