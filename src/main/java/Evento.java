5f83ddc6fd1b52b99fa517beb5f67e4b9b5ca938
Lima Limon <torsi@fi.uba.ar>
2023-04-22 13:16:25 -0300

    Merge del evento

--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -6,8 +6,6 @@ public class Evento extends Activities {
 
 	private LocalDateTime arranque;
 	private LocalDateTime termina;
-<<<<<<< HEAD
-	// private LocalDateTime ultimaDiaDelEvento;
 	private Frecuencia frecuencia;
 	
 	//--------- Atributos ---------
@@ -22,26 +20,6 @@ public class Evento extends Activities {
 	    // this.ultimaDiaDelEvento = this.frecuencia.finDeLasRepeticionesDadaFecha(this.arranque);
 	}
 
-=======
-
-	//--------- Atributos ---------
-
-	//--------- Constructores ---------
-	/*
-	* public Evento(LocalDateTime arranque, LocalDateTime termina) {
-		this.arranque = arranque;
-		this.termina = termina;
-	}
-	 */
-	public Evento(String name, String description, ArrayList<LocalDateTime> alarm, boolean isComplete, LocalDateTime arranque, LocalDateTime termina) {
-		super(name, description, alarm, isComplete);
-		this.arranque = arranque;
-		this.termina = termina;
-	}
-
-	//--------- Constructores ---------
-
->>>>>>> pre-main
 	//--------- Metodos ---------
 
 	@Override
@@ -56,25 +34,18 @@ public class Evento extends Activities {
 	public LocalDateTime cuandoTermina() {
 	    return this.termina;
 	}
-<<<<<<< HEAD
 
 	public boolean caeElDia(LocalDateTime diaEspecifico){
-	    //if (diaEspecifico.isAfter(this.ultimaDiaDelEvento) == true) {
-		//return false; //Si cae DESPUES del ultimo dia, entonces ni nos 
-			      ////molestamos en calcular si  la frecuencia hace 
-			      ////que caiga el dia que me piden
-		//}
-
 	    //Averiguo si la frecuencia hace que el evento caiga el dia pedido
 	    boolean caeElDiaPedido = this.frecuencia.dadoComienzoCaeElDia(this.arranque, diaEspecifico); 
 	    return caeElDiaPedido;
-=======
+	}
+
 	public void setArranque(LocalDateTime arranque) {
 		this.arranque = arranque;
 	}
 	public void setTermina(LocalDateTime termina) {
 		this.termina = termina;
->>>>>>> pre-main
 	}
 
 	//--------- Metodos ---------
