4c8bbcc71999780cd824474f3c3310a53403ca79
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-04-22 10:38:42 -0300

    Tests PlazoAnterior

--- src/main/java/PlazoAnterior.java
+++ src/main/java/PlazoAnterior.java
@@ -3,26 +3,26 @@ import java.time.Duration;
 public enum PlazoAnterior {
     DIAANTES("1 Dia Antes",Duration.ofDays(1L)),
     HORAANTES("1 Hora Antes",Duration.ofHours(1L)),
-    MEDIAHORAANTES("30 minutos Antes",Duration.ofMinutes(30L)),
-    QUINCEMINUTOSANTES("15 minutos Antes",Duration.ofMinutes(15L)),
-    DIEZMINUTOSANTES("10 minutos Antes",Duration.ofMinutes(10L)),
-    CINCOMINUTOSANTES("5 Dia Antes",Duration.ofMinutes(5L)),
+    MEDIAHORAANTES("30 Minutos Antes",Duration.ofMinutes(30L)),
+    QUINCEMINUTOSANTES("15 Minutos Antes",Duration.ofMinutes(15L)),
+    DIEZMINUTOSANTES("10 Minutos Antes",Duration.ofMinutes(10L)),
+    CINCOMINUTOSANTES("5 Minutos Antes",Duration.ofMinutes(5L)),
     ;
     private final String horarios;
     private final Duration tiempo;
-    private PlazoAnterior(String s,Duration t) {
+    PlazoAnterior(String s,Duration t) {
         horarios = s;
         tiempo = t;
     }
-
-
-    public Duration elHorarioEstablecido(String s){
-        Duration devolver = null;
+    public Duration elHorarioEstablecido(){
+        return tiempo;
+    }
+    public static PlazoAnterior compararHorariosDescriptos(String s){
         for (PlazoAnterior p: PlazoAnterior.values()){
             if (s.equals(p.horarios)){
-                devolver = p.tiempo;
+                return p;
             }
         }
-        return devolver;
+        return null;
     }
 }
