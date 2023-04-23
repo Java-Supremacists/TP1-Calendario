aed9000db33bd24fcd36c0d48c70bb6621fb1d85
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-04-23 17:48:12 -0300

    Comentarios para Implementar

--- src/main/java/Calendario.java
+++ src/main/java/Calendario.java
@@ -19,12 +19,101 @@ public class Calendario {
 
     //--------- Metodos ---------
     /*
+    private final ArrayList<Tarea> listaTareas = new ArrayList<>();
+    private final ArrayList<Evento> listaEventos = new ArrayList<>();
+
     public int crearTarea(LocalDateTime termina){
         Tarea nuevaTarea = new Tarea(termina);
         listaTareas.add(nuevaTarea);
-        """int retorno = IDActual;//estas lineas borradas, ya que no vamos a usar un ID-ACTUAL
-        IDActual++"""
         return nuevaTarea.hashCode();
+    }
+
+    public int crearEvento(LocalDateTime arranque, LocalDateTime termina){
+        Evento nuevoEvento = new Evento(arranque,termina);
+        listaEventos.add(nuevoEvento);
+        return nuevoEvento.hashCode();
+    }
+
+    public Activities obtenerActividad(int ID){
+        for (Evento e: listaEventos){
+            if (e.hashCode() == ID)){
+                return e;
+            }
+        }
+        for (Tarea t: listaTareas){
+            if (t.hashCode() == ID)){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public Evento obtenerEvento(int ID){
+        for (Evento e: listaEventos){
+            if (e.hashCode() == ID)){
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public Tarea obtenerTarea(int ID){
+        for (Tarea t: listaTareas){
+            if (t.hashCode() == ID)){
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public void modificarActividadNombre(int ID,String nombre){
+        var act = this.obtenerActividad(ID);
+        act.setName(nombre);
+    }
+
+    public void modificarActividadDescripcion(int ID, String descripcion){
+        var act = this.obtenerActividad(ID);
+        act.setDescription(descripcion);
+    }
+
+    public void modificarActividadEsDiaEntero(int ID, boolean esDiaCompleto){
+        var act = this.obtenerActividad(ID);
+        act.setComplete(esDiaCompleto);
+    }
+
+    public void modificarActividadAgregarAlarma(int ID, String duracion){
+        Duration tiempoAnterior = PlazoAnterior.elHorarioEstablecido(duracion);
+        var act = this.obtenerActividad(ID);
+        LocalDateTime arranca = act.cuandoEmpieza();
+        LocalDateTime alarma = arranca.minus(tiempoAnterior);
+        act.getAlarmas().agregarAlarma(alarma);
+    }
+
+    public void modificarActividadAgregarAlarma(int ID, LocalDateTime alarma){
+        var act = this.obtenerActividad(ID);
+        act.getAlarmas().agregarAlarma(alarma);
+    }
+
+    public void modificarActividadAgregarAlarma(int ID, LocalDateTime alarma){
+        var act = this.obtenerActividad(ID);
+        act.getAlarmas().agregarAlarma(alarma);
+    }
+
+    public modificarEventoPlazoTemporal(int ID, LocalDateTime arrancaNuevo, LocalDateTime terminaNuevo){
+        var ev = this.obtenerEvento(ID)
+        if (ev!= null){
+            ev.setArranque(arrancaNuevo);
+            ev.setTermina(terminaNuevo);
+        }
+    }
+
+    public modificarTareaPlazoTemporal(int ID,LocalDateTime terminaNuevo){
+        var t = this.obtenerTarea(ID)
+        if (t != null){
+            t.setTermina(terminaNuevo);
+        }
+    }
+
     //
     //Constructores con repeticion semanal
     public void crearEvento(String nombre, String description, ArrayList<LocalDateTime> alarm,  boolean esDiaCompleto,LocalDateTime arranque, LocalDateTime termina, Frecuencia frecuencia){
@@ -32,45 +121,6 @@ public class Calendario {
 	this.listaEventos.add(nuevoEvento);
     }
 
-    public int crearEvento(LocalDateTime arranque, LocalDateTime termina){
-        Evento nuevoEvento = new Evento( arranque,  termina);
-        listaEventos.add(nuevoEvento); //que sea un array
-        """int retorno = IDActual; //estas lineas borradas, ya que no vamos a usar un ID-ACTUAL
-        IDActual++"""
-        return nuevoEvento.hashCode();
-    }
-    public Activities obtenerActividad(int ID) throws IllegalAccessError{
-        for (Evento e: listaEventos){
-            if (e.hashCode() == ID)){
-                return e
-            }
-        }
-        for (Tarea t: listaEventos){
-            if (t.hashCode() == ID)){
-                return t
-            }
-        }
-        return null
-    }
-    public void modificarActividadNombre(int ID,String nombre){
-        var act = this.obtenerActividad(ID)
-        act.setName(nombre)
-    }
-    public void modificarActividadDescripcion(int ID, String descripcion){
-        var act = this.obtenerActividad(ID)
-        act.setDescription(descripcion)
-    }
-    public void modificarActividadEsDiaEntero(int ID, boolean esDiaCompleto){
-        var act = this.obtenerActividad(ID)
-        act.setComplete(esDiaCompleto)
-    }
-    public void modificarActividadAgregarAlarma(int ID, String duracion){
-        Duration tiempoAnterior = PlazoAnterior.elHorarioEstablecido(duracion)
-        var act = this.obtenerActividad(ID)
-        LocalDateTime arranca = act.cuandoEmpieza()
-        LocalDateTime alarma = arranca.minus(tiempoAnterior)
-        act.getAlarmas().agregarAlarma(alarma)
-    }
      */
     public int crearTarea(String nombre, String description, ArrayList<LocalDateTime> alarm,  boolean esDiaCompleto, LocalDateTime termina){
         Tarea nuevaTarea = new Tarea(nombre, description, alarm, esDiaCompleto, termina);
