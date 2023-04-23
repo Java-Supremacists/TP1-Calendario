2c001ce4eea4770da9789285b8eac76573f85292
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-04-23 09:42:06 -0300

    Actualizacion de Evento func

--- src/main/java/Alarmas.java
+++ src/main/java/Alarmas.java
@@ -33,6 +33,16 @@ public class Alarmas {
     public int size(){
         return alarmas.size();
     }
+    public void actualizarAlarmas(LocalDateTime dif){
+        if (dif != null && alarmas.size()==0 && mantenerAlarmas){
+            for (LocalDateTime alarm : alarmasYaSonadas){
+                LocalDateTime nuevaAlarm = alarm.plusYears(dif.getYear()).plusMonths(dif.getMonthValue()).plusDays(dif.getDayOfMonth()).plusHours(dif.getHour()).plusMinutes(dif.getMinute());
+                alarmasYaSonadas.remove(alarm);
+                alarmasYaSonadas.add(nuevaAlarm);
+            }
+            alarmas.addAll(alarmasYaSonadas);
+        }
+    }
     public void actualizarAlarmas(Duration cantidadASumar){
         if (cantidadASumar != null && alarmas.size()==0 && mantenerAlarmas){
             for (LocalDateTime alarm : alarmasYaSonadas){
--- src/main/java/Evento.java
+++ src/main/java/Evento.java
@@ -4,54 +4,75 @@ import java.util.ArrayList;
 public class Evento extends Activities {
 	//--------- Atributos ---------
 
-	private LocalDateTime arranque;
-	private LocalDateTime termina;
+	private LocalDateTime arranquePrincipio;
+	private LocalDateTime terminaPrincipio;
+	private LocalDateTime arranqueActual;
+	private LocalDateTime terminaActual;
 	private Frecuencia frecuencia;
-	
+
 	//--------- Atributos ---------
 
 	//--------- Constructores ---------
 	public Evento(String name, String description, ArrayList<LocalDateTime> alarm, boolean isComplete, LocalDateTime arranque, LocalDateTime termina, Frecuencia frecuencia) {
-	    super(name, description, alarm, isComplete);
-	    this.arranque = arranque;
-	    this.termina = termina;
-	    this.frecuencia = frecuencia;
+		super(name, description, alarm, isComplete);
+		this.arranquePrincipio = arranque;
+		this.arranqueActual = arranque;
+		this.terminaPrincipio = termina;
+		this.terminaActual = termina;
+		this.frecuencia = frecuencia;
 
-	    // this.ultimaDiaDelEvento = this.frecuencia.finDeLasRepeticionesDadaFecha(this.arranque);
+		// this.ultimaDiaDelEvento = this.frecuencia.finDeLasRepeticionesDadaFecha(this.arranque);
 	}
 
 	//--------- Metodos ---------
 
 	@Override
 	public tipo type() {
-	    return tipo.EVENTO;
+		return tipo.EVENTO;
 	}
 	@Override
 	public LocalDateTime cuandoEmpieza(){
-	    return this.arranque;
+		return this.arranqueActual;
 	}
 	@Override
 	public LocalDateTime cuandoTermina() {
-	    return this.termina;
+		return this.terminaActual;
 	}
-
 	public boolean caeElDia(LocalDateTime diaEspecifico){
-	    //Averiguo si la frecuencia hace que el evento caiga el dia pedido
-	    boolean caeElDiaPedido = this.frecuencia.dadoComienzoCaeElDia(this.arranque, diaEspecifico); 
-	    return caeElDiaPedido;
+		//Averiguo si la frecuencia hace que el evento caiga el dia pedido
+		boolean caeElDiaPedido = this.frecuencia.dadoComienzoCaeElDia(this.arranquePrincipio, diaEspecifico);
+		return caeElDiaPedido;
 	}
-
 	public LocalDateTime proximoEventoMasCercanoAFechaEspecifica(LocalDateTime diaEspecifico) {
-	    LocalDateTime proximoEvento;
-	    proximoEvento = this.frecuencia.proximoEventoMasCercanoAFechaEspecifica(this.arranque, diaEspecifico);
-	    return proximoEvento;
+		LocalDateTime proximoEvento;
+		proximoEvento = this.frecuencia.proximoEventoMasCercanoAFechaEspecifica(this.arranquePrincipio, diaEspecifico);
+		return proximoEvento;
 	}
-
+	public void actualizarEvento(){
+		if (LocalDateTime.now().isAfter(this.terminaActual)){
+			if (alarm.quedanAlarmas()){
+				while (alarm.quedanAlarmas()){
+					alarm.sonarAlarma();
+				}
+			}
+			var fechaAnteriorInicio = this.arranqueActual;
+			var fechaAnteriorFinal = this.terminaActual;
+			this.arranqueActual = this.proximoEventoMasCercanoAFechaEspecifica(fechaAnteriorInicio);
+			this.terminaActual = this.proximoEventoMasCercanoAFechaEspecifica(fechaAnteriorFinal);
+			/*if (){
+				alarm.actualizarAlarmas();
+			}else{*/
+			LocalDateTime diferenciaArranques = arranqueActual.minusYears(fechaAnteriorInicio.getYear()).minusMonths(fechaAnteriorInicio.getMonthValue()).minusDays(fechaAnteriorInicio.getDayOfMonth()).minusHours(fechaAnteriorInicio.getHour()).minusMinutes(fechaAnteriorInicio.getMinute());
+			alarm.actualizarAlarmas(diferenciaArranques);
+		}
+	}
 	public void setArranque(LocalDateTime arranque) {
-		this.arranque = arranque;
+		this.arranquePrincipio = arranque;
+		this.arranqueActual = arranque;
 	}
 	public void setTermina(LocalDateTime termina) {
-		this.termina = termina;
+		this.terminaPrincipio = termina;
+		this.terminaActual = termina;
 	}
 
 	//--------- Metodos ---------
