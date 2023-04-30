32b5226f84541baeeb14e3ec776211f9090207ca
Lima Limon <torsi@fi.uba.ar>
2023-04-30 15:31:50 -0300

    chequeo que astyle existe

--- formatear.sh
+++ formatear.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+which astyle &> /dev/null
+tenesAstyle=$(echo $?)
+if [ ${tenesAstyle} -ne 0 ]
+then
+	echo "No tenes astyle"
+	echo "Instalalo antes de seguir"
+	exit 2
+fi
+
+
 
 archivosSinComitear=$(git status -s | wc -l)
 
 if [ ${archivosSinComitear} -ne 0 ]
 then
-	echo "tenes cosas sin comitear"
+	echo "Tenes cosas sin comitear"
+	echo "Este script esta pensando para ser usado como unico commit"
+	echo "Deja todo comiteado y despues podes correr este programa."
+	exit 2
 fi
 
-echo ${archivosSinComitear}
 echo "Formateo los archivos main"
 astyle src/main/java/*
