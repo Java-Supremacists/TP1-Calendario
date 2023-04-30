6df6bca0a9b08b38be9877307e2f9e4c39ff84e2
Lima Limon <torsi@fi.uba.ar>
2023-04-30 12:35:11 -0300

    Saco comentarios de mas

--- src/test/java/EventoTest.java
+++ src/test/java/EventoTest.java
@@ -11,7 +11,6 @@ public class EventoTest {
     public void crearEventoVacio() {
         var arranca = LocalDateTime.of(2023,12,8,0,0);
         var termina = LocalDateTime.of(2023,12,9,0,0);
-        // var even1 = new Evento("","",null,false,arranca,termina,null);
         var even1 = new Evento(arranca,termina);
 
 
@@ -46,16 +45,12 @@ public class EventoTest {
         var termina = LocalDateTime.of(2023,12,8,0,0);
 
 
-        // TODO: Sacar comentario de la creacio nde la version anterior
-        // var even1 = new Evento("Nombre1","Descripcion1",alarmas,true,arranca,termina,null);
         var even1 = new Evento(arranca,termina);
         even1.setName("Nombre1");
         even1.setDescription("Descripcion1");
         even1.agregarAlarmas(alarmas);
         even1.setEsDiaCompleto(true);
 
-        // TODO: Sacar comentario de la creacio nde la version anterior
-        // var even2 = new Evento("Nombre2","Descripcion2",null,false,arranca,termina,null);
         var even2 = new Evento(arranca,termina);
         even2.setName("Nombre2");
         even2.setDescription("Descripcion2");
@@ -73,28 +68,7 @@ public class EventoTest {
         assertEquals(arranca, even2.cuandoEmpieza());
         assertEquals(termina, even2.cuandoTermina());
     }
-    @Test
-    public void CreacionEventosVariado() {
-    }
-    @Test
-    public void eventoCaeDiaPedido(){
 
-    /*arrange
-	//ArrayList<LocalDateTime> alarmas = new ArrayList<LocalDateTime>();
-	//of(int year, int month, int dayOfMonth, int hour, int minute) 
-	// Evento eventoDePrueba = new Evento("Evento de prueba", "Descripcion de prueba", alarmas, false, LocalDateTime.of(2023, 4, 10, 7, 45, 55), LocalDateTime.of(2024, 3, 10, 7, 45, 55));
-
-	//assert
-	// assertEquals(true, eventoDePrueba.caeElDia(LocalDateTime.of(2023, 4, 22, 7, 45, 55)));
-	// assertEquals(false, eventoDePrueba.caeElDia(LocalDateTime.of(2023, 4, 21, 7, 45, 55)));
-	// assertEquals(false, eventoDePrueba.caeElDia(LocalDateTime.of(2023, 4, 20, 7, 45, 55)));*/
-    }
-    @Test
-    public void cuandoEmpiezaTest(){
-    }
-    @Test
-    public void cuandoTerminaTest(){
-    }
     @Test
     public void UltimaAlarmaYSonarUltimaAlarma(){
         var arranca = LocalDateTime.of(2023,12,8,0,0);
@@ -161,7 +135,6 @@ public class EventoTest {
      
 	    var repeticionInfinita = new RepeticionInfinita();
 	    var frecuenciadiaria = new FrecuenciaDiaria(cadaCuantosdias, repeticionInfinita);
-	    //Copiar fechas de test de frecuencia
      
 	    var even1 = new Evento(arranca, termina);
 	    even1.setFrecuencia(frecuenciadiaria);
