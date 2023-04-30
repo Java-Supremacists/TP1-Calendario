5c969e4645bd08b3a06f0405f776a17d7af1b6bf
Lima Limon <torsi@fi.uba.ar>
2023-04-30 15:22:09 -0300

    chequeo igualdad

--- formatear.sh
+++ formatear.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+
+archivosSinComitear=$(git status -s | wc -l)
+
+if [ ${archivosSinComitear} -ne 0 ]
+then
+	echo "tenes cosas sin comitear"
+fi
+
+echo ${archivosSinComitear}
 echo "Formateo los archivos main"
 astyle src/main/java/*
