db759c78982eccb3a71be642cf254feaf162632d
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-05-28 12:48:02 -0300

    Escenas 1 y 2 cargadas

--- src/main/java/InterfazGrafica.java
+++ src/main/java/InterfazGrafica.java
@@ -1,15 +1,25 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class InterfazGrafica extends Application {
+    private Scene escena1;
+    private Scene escena2;
+
     @Override
     public void start(Stage stage) throws Exception {
-        var label = new Label("Hola mundo!");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("escenario1.fxml"));
+        FlowPane pantalla1 = loader.load();
+        escena1 = new Scene(pantalla1, 640, 480);
+
+        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("escenario2.fxml"));
+        FlowPane pantalla2 = loader2.load();
+        escena2 = new Scene(pantalla2, 640, 480);
+
+        stage.setScene(escena1);
         stage.show();
     }
 }
