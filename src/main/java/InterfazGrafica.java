b3ddcf583521d91053a7d8d619e6dc5f42e9deb6
FacuGerez <facundogerezuba@hotmail.com>
2023-05-28 18:59:46 -0300

    Modificacion de visualizacion Calendario

--- src/main/java/InterfazGrafica.java
+++ src/main/java/InterfazGrafica.java
@@ -14,12 +14,12 @@ public class InterfazGrafica extends Application {
         FXMLLoader loader1 = new FXMLLoader(getClass().getResource("escenario1.fxml"));
         loader1.setController(this);
         FlowPane pantalla1 = loader1.load();
-        escena1 = new Scene(pantalla1, 640, 480);
+        escena1 = new Scene(pantalla1, 854, 480);
 
         FXMLLoader loader2 = new FXMLLoader(getClass().getResource("escenario2.fxml"));
         loader2.setController(this);
         FlowPane pantalla2 = loader2.load();
-        escena2 = new Scene(pantalla2, 640, 480);
+        escena2 = new Scene(pantalla2, 754, 860);
 
         stage.setScene(escena1);
         stage.show();
