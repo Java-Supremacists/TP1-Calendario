77ba60a7d1019baf612656be42fcaa360b946738
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-06-14 12:02:23 -0300

    Empezando implementacion semanal

--- src/main/java/VistaDiaria.java
+++ src/main/java/VistaDiaria.java
@@ -6,7 +6,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
--- src/main/java/VistaSemanal.java
+++ src/main/java/VistaSemanal.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -64,6 +69,44 @@ public class VistaSemanal extends VistaCalendario {
     @Override
     public void visualizarActividades(List<Activities> hacerVisual,LocalDateTime fechaInicioSemana) {
         vaciarGrilla();
+        for (int i = 1; i<8;i++){
+            for (int j = 0; j<24;j++){
+                grillaDiasxHorarios.add(new GridPane(1,1),i,j);
+            }
+        }
+        int numChildren = grillaDiasxHorarios.getChildren().size();
+        for (int i = 0; i < numChildren; i++) {
+            var hijo = grillaDiasxHorarios.getChildren().get(i);
+            var columnIndex = GridPane.getColumnIndex(hijo);
+            var rowIndex = GridPane.getRowIndex(hijo);
+            //Hay un error aca, no funca
+            if (hacerVisual== null){return;}
+            for (Activities act : hacerVisual){
+                var comienza = act.cuandoEmpieza();
+                var termina = act.cuandoTermina();
+                var inicio = comienza.getHour();
+                var finaliza = comienza.getHour();
+                var dia = comienza.getDayOfWeek().getValue() % 7 + 1;
+                if (dia == columnIndex && inicio == rowIndex){
+                    Paint color = Color.web(VistaMensual.getRandomColor());
+                    var rectanguloColorido = new Rectangle(115,40,color);
+                    if (comienza.equals(termina)){
+                        //tarea
+                        var grilla = (GridPane) hijo;
+                        if (grilla.getColumnCount() == grilla.getChildren().size()){
+                            grilla.addColumn(grilla.getColumnCount(),rectanguloColorido);
+                        }else {
+                            grilla.add(rectanguloColorido,grilla.getColumnCount(),1);
+                        }
+                    }else {
+                        //evento
+                    }
+                }
+
+            }
+        }
+
+
     }
     private void vaciarGrilla() {
         grillaDiasxHorarios.getChildren().removeIf(e -> !e.getClass().equals(Label.class));
