de0d044ee8bf40dfba80ef3364a0ae6766ebe24d
Facundo Gerez <103956476+FacuGerez@users.noreply.github.com>
2023-06-14 23:09:48 -0300

    VistaDiaria casi terminada

--- src/main/java/VistaDiaria.java
+++ src/main/java/VistaDiaria.java
@@ -1,11 +1,19 @@
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -54,6 +62,42 @@ public class VistaDiaria extends VistaCalendario {
     @Override
     public void visualizarActividades(List<Activities> hacerVisual,LocalDateTime fechaHoy) {
         vaciarGrilla();
+        for (int i = 0; i <24; i++){
+            var celda = new HBox();
+            celda.setAlignment(Pos.CENTER_RIGHT);
+            grillaDiaxHora.add(celda,1, i);
+        }
+        if (hacerVisual== null){return;}
+        var listaDeLasFechasHoy = hacerVisual.stream().filter(j-> j.cuandoEmpieza().getDayOfMonth() == fechaHoy.getDayOfMonth()).toList();
+        for (Activities act : listaDeLasFechasHoy){
+            var inicio = act.cuandoEmpieza().getHour();
+            var finaliza = act.cuandoTermina().getHour();
+            Paint color = Color.web(VistaMensual.getRandomColor());
+            for (Node e : grillaDiaxHora.getChildren()) {
+                if (!e.getClass().equals(HBox.class)){continue;}
+                Integer row = GridPane.getRowIndex(e);
+                if (row != null && inicio <= row && row <= finaliza) {
+                    var hijo = (HBox) e;
+                    var rectangulo = new Rectangle((double) 800 / listaDeLasFechasHoy.size(), 40, color);
+                    rectangulo.setOnMouseClicked(mouseEvent -> {
+                        var vbox = new VBox();
+                        vbox.setAlignment(Pos.CENTER);
+                        if (act.cuandoEmpieza().equals(act.cuandoTermina())){
+                            var tarea = (Tarea) act;
+                             vbox.getChildren().addAll(new Label(tarea.getTitulo()),new Label(tarea.getDescripcion(),new Label(tarea.cuandoEmpieza().toString(),new Label(tarea.cuandoTermina().toString()))));
+                        }else {
+                            var evento = (Evento) act;
+                            vbox.getChildren().addAll(new Label(evento.getTitulo()),new Label(evento.getDescripcion()));
+                        }
+                        var stage = new Stage();
+                        stage.setScene(new Scene(vbox,331,249));
+                        stage.show();
+                    });
+                    hijo.getChildren().add(rectangulo);
+                }
+            }
+        }
+
     }
     private void vaciarGrilla() {
         grillaDiaxHora.getChildren().removeIf(e -> !e.getClass().equals(Label.class)); //significa que e es un elemento de visualizacion del evento o tarea
